bd80fc0f548b2c040d01151b197f75c2bacbfb39
Larry Golding <laurenceg@crosslaketech.com>
2022-06-21 11:41:49 -0700

    Fix: Don't use absolute URI for API endpoints.

--- src/services/TermService.ts
+++ src/services/TermService.ts
@@ -4,7 +4,7 @@ import ITermService from "./ITermService";
 export default class TermService implements ITermService {
   getAll = async () => {
     const promise: Promise<TermModel[]> = await (
-      await fetch(`http://localhost:7071/api/GetTerms`)
+      await fetch(`/api/GetTerms`)
     ).json();
     return promise;
   };
